d43d3cafb3734b4806758bcccb83c02338d3f228
Maximilian Antoni <mail@maxantoni.de>
2015-06-19 22:29:00 +0200

    Index all option fields and add $ as a shortcut for querySelector

--- test/edit-screen-choices-test.js
+++ test/edit-screen-choices-test.js
@@ -35,15 +35,27 @@ describe('edit-screen choices', function () {
     assert.equal(div.querySelectorAll('.option').length, 1);
   });
 
-  it('numbers option element names and title', function () {
-    div.querySelector('.add-option').click();
-    div.querySelector('.add-option').click();
-
+  function assertThreeOptions() {
     var titles = div.querySelectorAll('.option .title');
     assert.equal(titles[0].textContent, 'Option #1');
     assert.equal(titles[1].textContent, 'Option #2');
     assert.equal(titles[2].textContent, 'Option #3');
-    //assert.equal(div.querySelector('[name=option-text-1]'), expected);
+    assert.notEqual(div.querySelector('[name=option-text-1]'), null);
+    assert.notEqual(div.querySelector('[name=option-text-2]'), null);
+    assert.notEqual(div.querySelector('[name=option-text-3]'), null);
+    assert.notEqual(div.querySelector('[name=option-points-1]'), null);
+    assert.notEqual(div.querySelector('[name=option-points-2]'), null);
+    assert.notEqual(div.querySelector('[name=option-points-3]'), null);
+    assert.notEqual(div.querySelector('[name=next-screen-1]'), null);
+    assert.notEqual(div.querySelector('[name=next-screen-2]'), null);
+    assert.notEqual(div.querySelector('[name=next-screen-3]'), null);
+  }
+
+  it('numbers option element names and title', function () {
+    div.querySelector('.add-option').click();
+    div.querySelector('.add-option').click();
+
+    assertThreeOptions();
   });
 
   it('re-numbers existing option element names on remove', function () {
@@ -53,10 +65,7 @@ describe('edit-screen choices', function () {
 
     div.querySelectorAll('.remove-option')[0].click();
 
-    var titles = div.querySelectorAll('.option .title');
-    assert.equal(titles[0].textContent, 'Option #1');
-    assert.equal(titles[1].textContent, 'Option #2');
-    assert.equal(titles[2].textContent, 'Option #3');
+    assertThreeOptions();
   });
 
 });
